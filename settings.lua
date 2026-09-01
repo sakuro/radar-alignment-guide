43424b7f7c6f0f4951bf3e666ff39d89be41d238
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2026-09-01 20:30:49 +0900

    :sparkles: Add highlight color and map tag mod settings

---
+++ settings.lua
@@ -0,0 +1,14 @@
+data:extend({
+  {
+    type = "color-setting",
+    name = "radar-alignment-guide-highlight-color",
+    setting_type = "runtime-per-user",
+    default_value = {r = 0.1, g = 0.2, b = 0.5, a = 0.08},
+  },
+  {
+    type = "bool-setting",
+    name = "radar-alignment-guide-show-map-tag",
+    setting_type = "runtime-global",
+    default_value = false,
+  },
+})
